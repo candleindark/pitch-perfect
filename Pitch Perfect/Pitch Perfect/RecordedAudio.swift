1502749c183313404239f870f82c2ba164015612
candleindark <candleindark@gmail.com>
2015-04-27 06:22:39 -0500

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,16 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Isaac To on 4/27/15.
+//  Copyright (c) 2015 Isaac To. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject {
+    
+    var filePathUrl: NSURL!
+    var title: String!
+    
+}
